be8a197f131e2f96313a3c05428a394976e6a3d2
StrafeYosef <yosefplayz4580@gmail.com>
2023-06-14 11:37:48 +0300

    m

--- models/missionModel.js
+++ models/missionModel.js
@@ -1,45 +1,51 @@
 const mongoose = require("mongoose");
 
 const missionSchema = mongoose.Schema({
-  // givenIn: {
-  //   type: String,
-  //   required: true,
-  // },
   // token of user 
   token: {
     type: String,
     required: true,
   },
+  //כותרת הפגישה
   title: {
     type: String,
     required: true,
   },
+  //מועד הפגישה
   startedAt: {
     type: String,
     required: true,
   },
+  //ימים שנותרו
   daysLeft: {
     type: String,
     required: true,
   },
+  //פירוט המשימה
   details: {
     type: String,
     required: true,
   },
+  //אחראיות
   responsibility: {
     type: String,
     required: true,
   },
+  //תג"ב
   endedAt: {
     type: String,
     required: true,
   },
+  //סטטוס
   status: {
     type: String,
     required: true,
   },
+  //הערות אחראי
   noteResponsibility: String,
+  //הערות מפקד
   noteCommand: String,
+  //מזהה
   missionId: {
     type: String,
     required: true,

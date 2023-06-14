0922e0c6feb14f7ea9499490e84ea21a8ea1c278
moshe <moshe6073163@gmail.com>
2023-06-14 13:09:39 +0300

    M

--- routes/missionRoute.js
+++ routes/missionRoute.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { missionCtrl } = require("../controllers/missionCtrl");
 
-router.post("/", missionCtrl.addMission);
+router.post("/setMission", missionCtrl.addMission);
 router.get("/", missionCtrl.getAllMissions);
 
 module.exports = router;
